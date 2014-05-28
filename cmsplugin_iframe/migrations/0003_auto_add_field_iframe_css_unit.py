8514962d8cad0cc84f6130a178d70a555cc69fb2
Patrick Weber <pweber@spatialbit.com>
2014-05-28 11:18:20 +0200

    added css_unit choice between px and percentage for iframe width and height

---
+++ cmsplugin_iframe/migrations/0003_auto_add_field_iframe_css_unit.py
@@ -0,0 +1,54 @@
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'IFrame.css_unit'
+        db.add_column(u'cmsplugin_iframe', 'css_unit',
+                      self.gf('django.db.models.fields.CharField')(default='%', max_length=2),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'IFrame.css_unit'
+        db.delete_column(u'cmsplugin_iframe', 'css_unit')
+
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        u'cmsplugin_iframe.iframe': {
+            'Meta': {'object_name': 'IFrame', 'db_table': "u'cmsplugin_iframe'", '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'css_unit': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'height': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['cmsplugin_iframe']
--- cmsplugin_iframe/models.py
+++ cmsplugin_iframe/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from cms.models import CMSPlugin
 
 class IFrame(CMSPlugin):
+    UNITS = (
+        ('px','pixels'),
+        ('%','percentage'),
+    )
     title = models.CharField(max_length=255, null=True, blank=True)
     width = models.PositiveSmallIntegerField(blank=True, null=True)
     height = models.PositiveSmallIntegerField()
+    css_unit = models.CharField(max_length=2, choices=UNITS)
     url = models.URLField()
     
     def __unicode__(self):
