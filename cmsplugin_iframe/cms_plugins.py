c17c23c84f1690b75523c109b2d742fa09f050cf
Patrick Weber <pweber@spatialbit.com>
2014-10-01 10:45:16 +0200

    fixed missing import

--- cmsplugin_iframe/cms_plugins.py
+++ cmsplugin_iframe/cms_plugins.py
@@ -2,6 +2,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from models import IFrame
 from django.utils.translation import ugettext as _
+from django.conf import settings
 
 class IFramePlugin(CMSPluginBase):
     model = IFrame
